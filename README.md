59a92e20f58c930509beb60ccd3714e9c09b92eb
Ronald Haynes <ronaldhaynes@flexshopper.com>
2016-10-04 10:58:39 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mysql-replrepair
 Bash script to repair mysql replication due to duplicate entries error or key not found error
+
+I've utilized this script when repairing replication on a mysql slave that isn't in production that has failed replicating.
+
+The three conditions are as follows:
+
+1. Seconds Behind Master = Null
+
+2. Last Error known = Duplicate Entry
+
+3. Last Error known = Key not found
+
+This script should ouly be used after mysql db has been restored and the original error should be diagnosed before using this script. 
+Use at your own risk :) 
+
+
---
+++ sql_repl_repair.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+logfile=~/slave-watcher.log
+while true; do
+    status=$(mysql --execute="show slave status\G"|grep "Seconds_Behind_Master:"|awk '{print $2}')
+    dup_err=$(mysql --execute="show slave status\G"|grep "Last_SQL_Error:"|grep -oh 'Duplicate')
+    delete_err=$(mysql --execute="show slave status\G"|grep "Last_SQL_Error:"|grep -oh 'HA_ERR_KEY_NOT_FOUND')
+    if [[ $status == "NULL" || $dup_err == "Duplicate" || $delete_err == "HA_ERR_KEY_NOT_FOUND"  ]]; then
+        mysql --execute="show slave status\G" | grep "Last_SQL_Error:" | tee -a $logfile
+        mysql --execute="set global sql_slave_skip_counter=1; start slave;"
+    fi
+    sleep 1
+done
